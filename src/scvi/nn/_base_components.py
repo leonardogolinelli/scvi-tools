ac1fdc10f0a596ec86561bbc78e267653f56b755
leonardogolinelli <leogolinelli@gmail.com>
2025-05-05 10:30:47 +0200

    added velocity decoder, missing velocity loss function and 2 regime training plan

--- src/scvi/nn/_base_components.py
+++ src/scvi/nn/_base_components.py
@@ -541,6 +541,82 @@ class MaskedLinearDecoder(nn.Module):
         px_rate = torch.exp(library) * px_scale
         px_dropout = None
         return px_scale, px_r, px_rate, px_dropout
+    
+class VelocityDecoder(nn.Module):
+    """Decodes data from latent space to data space.
+
+    ``n_input`` dimensions to ``n_output``
+    dimensions using a fully-connected neural network of ``n_hidden`` layers.
+    Output is the mean and variance of a multivariate Gaussian
+
+    Parameters
+    ----------
+    n_input
+        The dimensionality of the input (latent space)
+    n_output
+        The dimensionality of the output (data space)
+    n_cat_list
+        A list containing the number of categories
+        for each category of interest. Each category will be
+        included using a one-hot encoding
+    n_layers
+        The number of fully-connected hidden layers
+    n_hidden
+        The number of nodes per hidden layer
+    dropout_rate
+        Dropout rate to apply to each of the hidden layers
+    kwargs
+        Keyword args for :class:`~scvi.module._base.FCLayers`
+    """
+
+    def __init__(
+        self,
+        n_input: int,
+        n_output: int,
+        n_cat_list: Iterable[int] = None,
+        n_layers: int = 1,
+        n_hidden: int = 128,
+        **kwargs,
+    ):
+        
+        super().__init__()
+        self.decoder = FCLayers(
+            n_in=n_input,
+            n_out=n_hidden,
+            n_cat_list=n_cat_list,
+            n_layers=n_layers,
+            n_hidden=n_hidden,
+            n_output=n_input,
+            dropout_rate=0,
+            **kwargs,
+        )
+
+        self.velocity_decoder = nn.Linear(n_hidden, n_output)
+
+    def forward(self, x: torch.Tensor, *cat_list: int):
+        """The forward computation for a single sample.
+
+         #. Decodes the data from the latent space using the decoder network
+         #. Returns tensors for the mean and variance of a multivariate distribution
+
+        Parameters
+        ----------
+        x
+            tensor with shape ``(n_input,)``
+        cat_list
+            list of category membership(s) for this sample
+
+        Returns
+        -------
+        2-tuple of :py:class:`torch.Tensor`
+            Mean and variance tensors of shape ``(n_output,)``
+
+        """
+        # Parameters for latent distribution
+        p = self.decoder(x, *cat_list)
+        velocity = self.velocity_decoder(p)
+        return velocity
+
 
 # Decoder
 class Decoder(nn.Module):
